7c9a555526f58ddd4ed941958884a5c84d3518cb
erikarg <erikatrue@gmail.com>
2023-01-19 14:48:13 -0300

    feat: add java folder with 5 solved challenges

---
+++ Java/CalcularDoces.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// Desafio
+// Uma lojinha de doces tem muitos clientes por ser ao lado de uma escola.
+// Para aumentar as vendas o dono resolveu colocar uma máquina onde os clientes,
+// principalmente as crianças que estão muito acostumadas com a tecnologia, poderiam
+// colocar suas moedas e receber dois doces aleatórios. Cada 1 real irá render 2 doces aleatórios.
+
+// Entrada
+// A entrada será a quantidade de dinheiro que cada cliente, um por vez, possui.
+
+// Saída
+// A saída deverá ser a quantidade de doces que cada cliente conseguiu obter. (sem as aspas)
+
+public class CalcularDoces {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+    int doce = leitor.nextInt();
+    leitor.close();
+
+    int qntDoces = 2;
+    int output = doce * qntDoces;
+    System.out.println("O cliente obteve " + output + " doces");
+  }
+
+}
